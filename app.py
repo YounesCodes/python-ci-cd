f68ac9bddf4df289a940d8e6074c05f4906f9d1b
YounesCodes <younesbouatmani1@gmail.com>
2026-04-05 01:28:21 +0100

    adding work done in app return JSON

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return {"status": "ok"}
+    return {
+        "message":"app is working.",
+        "work done":"contenarized the app, pushed to github."
+        # next step is to write ci/cd pipeline.
+    }
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
