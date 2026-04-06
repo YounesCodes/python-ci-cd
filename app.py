96dfb9c565e569ce575b83a581f01dc1f44b4c43
YounesCodes <younesbouatmani1@gmail.com>
2026-04-06 12:36:10 +0100

    added deployment to EC2 via github actions pipeline

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return {
-        "05/04/2026":"containerized the app, pushed to github, added github actions workflow to build the image, extended the workflow to also push image to docker hub, added a test stage to complement the pipeline."
+        "05/04/2026": "containerized the app, pushed to github, added github actions workflow to build the image, extended the workflow to also push image to docker hub, added a test stage to complement the pipeline.",
+        "06/04/2026": "extended the workflow to deploy app on EC2 instance."
     }
 
 if __name__ == "__main__":
