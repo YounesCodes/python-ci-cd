6d1ec8bc35c8400a7b553c1762e87e4ca0027154
YounesCodes <younesbouatmani1@gmail.com>
2026-04-08 23:09:10 +0100

    added semgrep SAST to pipeline

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def index():
     return {
         "05/04/2026": "containerized the app, pushed to github, added github actions workflow to build the image, extended the workflow to also push image to docker hub, added a test stage to complement the pipeline.",
         "06/04/2026": "extended the workflow to deploy app on EC2 instance.",
-        "08/04/2026": "added security scanning with Trivy in the pipeline."
+        "08/04/2026": "added security scanning with Trivy in the pipeline + semgrep SAST"
     }
 
 if __name__ == "__main__":
