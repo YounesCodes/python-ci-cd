f625dc5dc4f0ac1d728135d640c50954f0e49107
YounesCodes <younesbouatmani1@gmail.com>
2026-04-08 23:56:36 +0100

    forgot to boot up instance for pipeline completion

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def index():
     return {
         "05/04/2026": "containerized the app, pushed to github, added github actions workflow to build the image, extended the workflow to also push image to docker hub, added a test stage to complement the pipeline.",
         "06/04/2026": "extended the workflow to deploy app on EC2 instance.",
-        "08/04/2026": "added security scanning with Trivy in the pipeline + semgrep SAST"
+        "08/04/2026": "added security scanning with Trivy in the pipeline + semgrep SAST + fixed findings flagged by both"
     }
 
 if __name__ == "__main__":
