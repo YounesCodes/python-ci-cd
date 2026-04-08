5d3f819b0a0025d1431093ae456ecdd3ba4870f3
YounesCodes <younesbouatmani1@gmail.com>
2026-04-09 00:14:25 +0100

    changed ssh commands to delete old images and only keep latest on instance

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,7 +8,8 @@ def index():
     return {
         "05/04/2026": "containerized the app, pushed to github, added github actions workflow to build the image, extended the workflow to also push image to docker hub, added a test stage to complement the pipeline.",
         "06/04/2026": "extended the workflow to deploy app on EC2 instance.",
-        "08/04/2026": "added security scanning with Trivy in the pipeline + semgrep SAST + fixed findings flagged by both"
+        "08/04/2026": "added security scanning with Trivy in the pipeline + semgrep SAST + fixed findings flagged by both.",
+        "09/04/2026": "modified ssh commands in pipeline to delete old images from instance and only keep latest to save storage."
     }
 
 if __name__ == "__main__":
